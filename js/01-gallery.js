eb60856c911833e12e81202c0a8bc75af181a2d3
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-13 21:10:39 +0200

    Задание__1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallerymarkup = onAddGallery(galleryItems);
+let galleryOriginal;
+
+function onAddGallery(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="${original}" onclick="return false">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+          </a>
+          </div>`
+    )
+    .join("");
+}
+
+function openGalleryOriginal(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  addGalleryOriginal(e.target.dataset.source);
+
+  galleryOriginal.show();
+
+  document.addEventListener("keydown", closeGalleryOriginal);
+}
+
+function addGalleryOriginal(gallery) {
+  galleryOriginal = basicLightbox.create(`<img src="${gallery}">`);
+}
+
+function closeGalleryOriginal(e) {
+  if (e.code === "Escape") {
+    galleryOriginal.close();
+    document.removeEventListener("keydown", closeGalleryOriginal);
+  }
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", gallerymarkup);
+galleryContainer.addEventListener("click", openGalleryOriginal);
