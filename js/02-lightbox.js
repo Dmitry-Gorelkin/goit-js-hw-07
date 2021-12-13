92cbb3786fc56fabb0b2ae5944198898c8c3ca10
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-13 22:23:42 +0200

    Задание__2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallerymarkup = onAddGallery(galleryItems);
+
+function onAddGallery(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}" onclick="return false">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", gallerymarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
